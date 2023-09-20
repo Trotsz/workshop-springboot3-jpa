4312c4ac02099f94b974d5d4765f6115fa87ead8
Joao Pedro <jpviolao@hotmail.com>
2023-09-20 02:42:48 -0300

    Added H2 database, JPA and test profile

--- src/main/java/com/pursuit/springclass/entities/User.java
+++ src/main/java/com/pursuit/springclass/entities/User.java
@@ -1,8 +1,15 @@
 package com.pursuit.springclass.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name="tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
